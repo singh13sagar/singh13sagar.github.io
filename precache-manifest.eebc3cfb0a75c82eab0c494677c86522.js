4d71aaaa0ff38953fd58d327057afb41c6ba8603
Sagar Singh <singh13sagar@gmail.com>
2020-07-30 02:46:44 -0400

    Creating react app

---
+++ precache-manifest.eebc3cfb0a75c82eab0c494677c86522.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94259ae75b196e9263cdca69c6bc6bd",
+    "url": "/kirtananmolratan/index.html"
+  },
+  {
+    "revision": "3a1c5ef1390ca23c4640",
+    "url": "/kirtananmolratan/static/css/main.61a22e17.chunk.css"
+  },
+  {
+    "revision": "672714b704e37c0a1617",
+    "url": "/kirtananmolratan/static/js/2.4c36d520.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/kirtananmolratan/static/js/2.4c36d520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1c5ef1390ca23c4640",
+    "url": "/kirtananmolratan/static/js/main.9b67ecd8.chunk.js"
+  },
+  {
+    "revision": "8f90c12db043214751f5",
+    "url": "/kirtananmolratan/static/js/runtime-main.88c916bf.js"
+  },
+  {
+    "revision": "f74c4e60dcca6e0761e485c70e6bb085",
+    "url": "/kirtananmolratan/static/media/babashaamsingh.f74c4e60.jpg"
+  },
+  {
+    "revision": "d057acb05b3cc99336edb21fb3c9bb85",
+    "url": "/kirtananmolratan/static/media/bhaiaghafaiz.d057acb0.jpg"
+  },
+  {
+    "revision": "8fcc3de0157515c480c0bd98ddb3038f",
+    "url": "/kirtananmolratan/static/media/bhaiarjansinghtarangar.8fcc3de0.jpg"
+  },
+  {
+    "revision": "d94824d2e35129f459a55091efb50f42",
+    "url": "/kirtananmolratan/static/media/bhaibahadursingh.d94824d2.jpg"
+  },
+  {
+    "revision": "d5b18417c252e0847c41890e7b522711",
+    "url": "/kirtananmolratan/static/media/bhaibhaagsingh.d5b18417.jpg"
+  },
+  {
+    "revision": "cdd4ce4b802b9841949909f12113dc25",
+    "url": "/kirtananmolratan/static/media/bhaiboora.cdd4ce4b.jpg"
+  },
+  {
+    "revision": "96ea29964e837a3a47a6eaa6c026cd1e",
+    "url": "/kirtananmolratan/static/media/bhaichand.96ea2996.jpg"
+  },
+  {
+    "revision": "bfa046c4cb13e78b7e85fda6e01cbb03",
+    "url": "/kirtananmolratan/static/media/bhaighasita.bfa046c4.jpg"
+  },
+  {
+    "revision": "34932c0bfa20146bd17d561b53c6ddbc",
+    "url": "/kirtananmolratan/static/media/bhaigurmukhsinghfakkar.34932c0b.jpg"
+  },
+  {
+    "revision": "927266977edff3c0f48ea7c19c064fa2",
+    "url": "/kirtananmolratan/static/media/bhaihirasingh.92726697.jpg"
+  },
+  {
+    "revision": "6f18bc5d357a6fb6309914e2a5d079c5",
+    "url": "/kirtananmolratan/static/media/bhaijwalasinghthattatibba.6f18bc5d.jpg"
+  },
+  {
+    "revision": "9977fa330cac0c52cdfaa11cfbf55c1c",
+    "url": "/kirtananmolratan/static/media/bhailal.9977fa33.jpg"
+  },
+  {
+    "revision": "05d691843b72379f9c9bf48cff655989",
+    "url": "/kirtananmolratan/static/media/bhaipalsingh.05d69184.jpg"
+  },
+  {
+    "revision": "b7122125b4edef2b85785c9bc151539c",
+    "url": "/kirtananmolratan/static/media/bhairoora.b7122125.jpg"
+  },
+  {
+    "revision": "3cd272b00a5c9a4374942f9ef59536c9",
+    "url": "/kirtananmolratan/static/media/bhaisamundsingh.3cd272b0.jpg"
+  },
+  {
+    "revision": "feae36e5c3356660ab315fc0990e23b1",
+    "url": "/kirtananmolratan/static/media/bhaisantasingh.feae36e5.jpg"
+  },
+  {
+    "revision": "b0be7170d87dab4e58cad29615c324d2",
+    "url": "/kirtananmolratan/static/media/bhaisantu.b0be7170.jpg"
+  },
+  {
+    "revision": "f9c0399a3c5b3596792a9f51194f7674",
+    "url": "/kirtananmolratan/static/media/bhaisarmukhsinghfakkar.f9c0399a.jpg"
+  },
+  {
+    "revision": "f9908cd0fda043dd59f649650e7496cb",
+    "url": "/kirtananmolratan/static/media/bhaisudhsingh.f9908cd0.jpg"
+  },
+  {
+    "revision": "7c1c1ebc4c6061468037a6b806319e2a",
+    "url": "/kirtananmolratan/static/media/bhaisundarsingh.7c1c1ebc.jpg"
+  },
+  {
+    "revision": "55c75b26b3e0cc85812ba1a3015aa5fc",
+    "url": "/kirtananmolratan/static/media/bhaisurjansingh.55c75b26.jpg"
+  },
+  {
+    "revision": "e4cb8c0f57dc595a2337f3fad72b94be",
+    "url": "/kirtananmolratan/static/media/bhaitaba.e4cb8c0f.jpg"
+  },
+  {
+    "revision": "0ea59cfe256b1516c03611d7b41e55d8",
+    "url": "/kirtananmolratan/static/media/giansinghalmast.0ea59cfe.jpg"
+  },
+  {
+    "revision": "e7e497d6d29f905fb30c1e9d3ed431d6",
+    "url": "/kirtananmolratan/static/media/homepage.e7e497d6.jpg"
+  },
+  {
+    "revision": "07150ca9b4ae3f616db980d22c53665a",
+    "url": "/kirtananmolratan/static/media/mahantgajjasingh.07150ca9.jpg"
+  },
+  {
+    "revision": "3ae222f07b9737a56f8e63aabd95adff",
+    "url": "/kirtananmolratan/static/media/profdarshansinghkomal.3ae222f0.jpg"
+  }
+]);
